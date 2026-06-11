3ef8533bfc01958fd79210537f7197834a814b91
opelmanz <paulhitchings1@gmail.com>
2026-06-10 17:20:50 -0700

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const LA_LON = -118.2437;
 
 let results = [];
 
-document.getElementById("debugBanner").textContent = "moved API debug section";
+document.getElementById("debugBanner").textContent = "location width adjustment";
 console.log("APP JS LOADED ✔");
 
 window.runSearch = runSearch;
