57d0f5248911d791c305debfe30f18f13dc4ea9c
opelmanz <paulhitchings1@gmail.com>
2026-06-10 17:19:00 -0700

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const LA_LON = -118.2437;
 
 let results = [];
 
-document.getElementById("debugBanner").textContent = "fixed big crash";
+document.getElementById("debugBanner").textContent = "moved API debug section";
 console.log("APP JS LOADED ✔");
 
 window.runSearch = runSearch;
