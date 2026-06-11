78d2511f1845ed20f1f7ecb902d1142f7dda1ed0
opelmanz <paulhitchings1@gmail.com>
2026-06-10 18:18:58 -0700

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const LA_LON = -118.2437;
 
 let results = [];
 
-document.getElementById("debugBanner").textContent = "Column width adjustment 1";
+document.getElementById("debugBanner").textContent = "Column width adjustment 2";
 console.log("APP JS LOADED ✔");
 
 window.runSearch = runSearch;
