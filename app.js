3cbd42e1f831a31f2e1db2008a66b3eca9cf65e9
opelmanz <paulhitchings1@gmail.com>
2026-06-10 19:11:15 -0700

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const LA_LON = -118.2437;
 
 let results = [];
 
-document.getElementById("debugBanner").textContent = "Category box to 100% width, 50vh height";
+document.getElementById("debugBanner").textContent = "Debug notes - Category box to 100% width, 50vh height v2";
 console.log("APP JS LOADED ✔");
 
 window.runSearch = runSearch;
