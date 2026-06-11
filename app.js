a68340f4e17848a3567cd4081ff59a14bca34480
opelmanz <paulhitchings1@gmail.com>
2026-06-10 18:00:31 -0700

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const LA_LON = -118.2437;
 
 let results = [];
 
-document.getElementById("debugBanner").textContent = "API source test 2";
+document.getElementById("debugBanner").textContent = "API source test 3";
 console.log("APP JS LOADED ✔");
 
 window.runSearch = runSearch;
